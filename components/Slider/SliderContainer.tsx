63d96aba946ecd2f116918adf98ca64e6fe856b3
Prathamesh Gonjari <64478167+PrathameshGonjari@users.noreply.github.com>
2023-09-08 23:47:35 +0530

    slider label style adjusted

--- components/Slider/SliderContainer.tsx
+++ components/Slider/SliderContainer.tsx
@@ -31,23 +31,21 @@ const SliderContainer = ({
   return (
     <>
       <div className="w-full overflow-hidden">
-        <div className="relative">
-          {slides.map((slide, index) => {
-            return (
-              <div
-                key={index}
-                className={`${currentSlide === index ? "block" : "hidden"
-                  } transition delay-150 float-left w-full duration-600 ease-in-out`}
-              >
-                <img src={slide.imageUrl} className="block w-full" alt="..." />
-                <div className="absolute inset-x-0 bottom-5 py-5 text-center text-white">
-                  <h5 className="text-xl">{slide.label}</h5>
-                  <p>{slide.content}</p>
-                </div>
+        {slides.map((slide, index) => {
+          return (
+            <div
+              key={index}
+              className={`${currentSlide === index ? "block" : "hidden"
+                } transition delay-150 float-left w-full duration-600 ease-in-out`}
+            >
+              <img src={slide.imageUrl} className="  block w-full" alt="imageUrl" />
+              <div className="absolute inset-x-0 bottom-5 py-5 text-center text-white">
+                <h5 className="text-xl">{slide.label}</h5>
+                <p>{slide.content}</p>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
       <SliderButton
         onClick={prevSlide}
